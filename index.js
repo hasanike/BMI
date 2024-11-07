d73cf8ed769e881bb9fe81a9254c11e9ed6bb724
hkeidenb <hasani.eidenberger@gmail.com>
2024-11-07 14:53:49 -0800

    conditions added

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 let weight;
 let height; 
-let userBMI
+let userBMI;
 
+if (UserBMI < 18.5) {
+console.log("BMI is low")
+}
+else if (UserBMI < 25) {
+console.log("BMI is Normal")
+}
+else ( UserBMI< 30 )
+console.log("BMi is High")
+if (userBMI > 30)
+console.log("BMI suggests Obesity");
